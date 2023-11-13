2f62753f584baa648c929074fbc45bdd1b5b31c7
Kaushik Kundu <kaushikkundu774@gmail.com>
2023-11-13 21:01:14 +0530

    day 8

--- src/Recursion/reverseNum.java
+++ src/Recursion/reverseNum.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class reverseNum {
     public static void main(String[] args) {
-         reverse(1234);
+         int rev = reverse2(1234);
         System.out.println(rev);
     }
     static int rev = 0;
@@ -15,4 +15,17 @@ public class reverseNum {
          reverse(n/10);
 
     }
+    // second approach
+    static int reverse2(int n){
+        int digits = (int)(Math.log10(n) + 1);
+        return helper(n,digits);
+    }
+    static int helper(int n, int arg){
+        if(n % 10 == n){
+           return n;
+        }
+        int rem = n % 10;
+        return (int) (rem * Math.pow(10, arg - 1) + helper(n/10, arg -1));
+    }
+
 }
