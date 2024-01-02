95adba1f200090174503c30bdc855741d34152e6
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-01-02 15:25:23 +0530

    first commit

---
+++ src/Array/ArrayPermutation.java
@@ -0,0 +1,15 @@
+package Array;
+
+class ArrayPermutation {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+                nums[i] = nums[i] + (n * (nums[nums[i]]% n));
+
+        }
+        for(int j=0; j<n; j++){
+            nums[j] /= n;
+        }
+        return nums;
+    }
+}
