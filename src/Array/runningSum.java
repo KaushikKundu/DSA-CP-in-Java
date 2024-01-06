80650d877d1a1835c549c439dca65d661d6a2fc2
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-01-06 10:37:43 +0530

    day 2

---
+++ src/Array/runningSum.java
@@ -0,0 +1,13 @@
+package Array;
+
+class RunningSum {
+    public int[] runningSum(int[] nums) {
+        int[] arr = new int[nums.length];
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+             sum += nums[i];
+            arr[i] = sum;
+        }
+        return  arr;
+    }
+}
