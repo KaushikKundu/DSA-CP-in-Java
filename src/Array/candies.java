a075ffc9e8c32030f544ed01e8a35924531a43a3
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-01-06 22:20:06 +0530

    day 2

---
+++ src/Array/candies.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Candies {
+public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        List<Boolean> ans = new ArrayList<>();
+        for(int i=0; i<candies.length; i++){
+        if(candies[i] > max){
+        max = candies[i];
+        }
+        }
+        for(int i=0; i<candies.length; i++){
+        int val = candies[i] + extraCandies;
+        if( val >= max){
+        ans.add(true);
+        }else{
+        ans.add(false);
+        }
+        }
+        return ans;
+        }
+        }
